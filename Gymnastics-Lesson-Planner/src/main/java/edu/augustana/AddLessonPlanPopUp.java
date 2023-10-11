7e6c708295198fbd8fe2213f8171b1aa8b7cdf82
aakriti-bhandari <aakritibhandari22@augustana.edu>
2023-10-11 17:34:19 -0500

    created ui for the pop up windows to create lesson plans

---
+++ Gymnastics-Lesson-Planner/src/main/java/edu/augustana/AddLessonPlanPopUp.java
@@ -0,0 +1,23 @@
+package edu.augustana;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AddLessonPlanPopUp {
+
+   @FXML
+    private Button createNewLessonPlan;
+
+   @FXML
+    private void initialize(){
+
+   }
+
+//   @FXML
+//    private void createNewLessonPlanAction(ActionEvent event){
+//       App.createNewLessonPlanPopUp();
+//   }
+
+
+}
--- Gymnastics-Lesson-Planner/src/main/java/edu/augustana/App.java
+++ Gymnastics-Lesson-Planner/src/main/java/edu/augustana/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,10 +29,11 @@ public class App extends Application {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+   private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+
 
     public static void main(String[] args) {
         launch();
